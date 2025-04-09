5014ecb21acbd962d23c24c9fc06baa203ea329a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-04-09 13:20:30 +0530

    update

---
+++ src/main/java/org/example/controler/operation.java
@@ -0,0 +1,16 @@
+package org.example.controler;
+
+
+public class operation {
+
+    // creating the function of the addition and multiple
+    public int add(int a , int b){
+        return a+b ;
+    }
+
+    // creating another function of multiple\
+    public int multiplication(int a , int b){
+        return a*b ;
+    }
+
+}
